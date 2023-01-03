24fe7bb51255f6ba46c8a0b65eb130ea93c7c3b4
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-01-03 21:50:28 +1000

    Add files via upload

---
+++ 2016DodgeCharger/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+games { 'gta5' }
+author 'Andrew Development'
+
+files {
+    'data/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
